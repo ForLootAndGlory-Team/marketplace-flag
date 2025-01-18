17a45201a2aac7e1c76edfb064f0d2a2faafc054
Drikkx <88060062+Drikkx@users.noreply.github.com>
2025-01-18 10:02:09 +0100

    add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
-
-})
+	plugins: [react(),
+	VitePWA({
+		registerType: 'autoUpdate',
+		includeAssets: ['favicon.ico'],
+		manifest: {
+			name: 'ForLootAndGlory',
+			short_name: 'FLAG',
+			description: 'Game For Loot And Glory',
+			theme_color: '#ffffff',
+			icons: [
+				{
+					src: '/images/logo/flag_logo_round.png',
+					sizes: '192x192',
+					type: 'image/png'
+				},
+				// Ajoutez d'autres tailles ici
+			]
+		},
+		workbox: {
+			globPatterns: ['*/.{js,css,html,ico,png,svg}'],
+		},
+		devOptions: {
+			enabled: true
+		}
+	}),
+	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	build: {
+		// En production, assurez-vous que tous les chemins mènent à index.html
+		outDir: 'dist',
+		emptyOutDir: true,
+	},
+});
